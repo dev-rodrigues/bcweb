825e69295e8735c6b86b384215319b37056b2456
Carlos Henrique Dos Santos Rodrigues Junior <rodrigues.carlos@ifood.com.br>
2024-02-06 15:41:31 -0300

    wip

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import "./global.css"
+import {Button} from "@/components/ui/button.tsx";
+
+export function App() {
+    return (
+        <div className="flex justify-center items-center h-screen">
+           <Button>
+                Hello World
+           </Button>
+        </div>
+    )
+}
