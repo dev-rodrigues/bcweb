1b4bb29169be10dd8ff3334158c8daf6046515c7
Carlos Henrique Dos Santos Rodrigues Junior <rodrigues.carlos@ifood.com.br>
2024-02-15 15:45:20 -0300

    feat(master): wip

--- src/pages/app/dashboard.tsx
+++ src/pages/app/dashboard.tsx
@@ -1,10 +1,18 @@
 import { Helmet } from 'react-helmet-async'
 
+import { MonthRevenueUsersCard } from '@/pages/app/dashboard/month-revenue-users-card.tsx'
+
 export function Dashboard() {
   return (
     <>
       <Helmet title={'Dashboard'} />
-      <h1>Dashboard</h1>
+      <div className="flex flex-col gap-4">
+        <h1 className="text-3xl font-bold tracking-tight">Dashboard</h1>
+
+        <div className={'grid grid-cols-4 gap-4'}>
+          <MonthRevenueUsersCard />
+        </div>
+      </div>
     </>
   )
 }
---
+++ src/pages/app/dashboard/month-revenue-users-card.tsx
@@ -0,0 +1,28 @@
+import { Users } from 'lucide-react'
+
+import {
+  Card,
+  CardContent,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card.tsx'
+
+export function MonthRevenueUsersCard() {
+  return (
+    <Card>
+      <CardHeader className="flex-row items-center justify-between space-y-0 pb-2">
+        <CardTitle className="text-base font-semibold">
+          Total de times
+        </CardTitle>
+        <Users className="h-4 w-4 text-muted-foreground" />
+      </CardHeader>
+      <CardContent className="space-y-1">
+        <span className="text-2xl font-semibold tracking-tight">1000</span>
+        <p className="text-xs text-muted-foreground">
+          <span className="text-emerald-500 dark:text-emerald-400">+2%</span> em
+          relação ao mês passado
+        </p>
+      </CardContent>
+    </Card>
+  )
+}
