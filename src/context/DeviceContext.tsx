6020cdc13dd5f137463e8eaf0ee98127aa895f90
Carlos Henrique Dos Santos Rodrigues Junior <rodrigues.carlos@ifood.com.br>
2024-05-13 14:18:49 -0300

    fix mobile

--- src/context/DeviceContext.tsx
+++ src/context/DeviceContext.tsx
@@ -10,13 +10,23 @@ const DeviceProvider: React.FC<DeviceProviderProps> = ({ children }) => {
   const [isMobile, setIsMobile] = useState(false)
 
   useEffect(() => {
-    const userAgent =
-      typeof window.navigator === 'undefined' ? '' : navigator.userAgent
-    const mobile =
-      /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
-        userAgent,
-      )
-    setIsMobile(mobile)
+    const checkMobile = () => {
+      const userAgent =
+        typeof window.navigator === 'undefined' ? '' : navigator.userAgent
+      const mobile =
+        /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(
+          userAgent,
+        )
+      setIsMobile(mobile)
+    }
+
+    checkMobile()
+
+    window.addEventListener('resize', checkMobile)
+
+    return () => {
+      window.removeEventListener('resize', checkMobile)
+    }
   }, [])
 
   return (
