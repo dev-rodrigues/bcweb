9768face6884b5d9ffaf33b8f37672618754080e
Carlos Henrique Dos Santos Rodrigues Junior <rodrigues.carlos@ifood.com.br>
2024-05-13 14:24:28 -0300

    add analytics

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,4 @@
+import { Analytics } from '@vercel/analytics/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 
@@ -6,5 +7,6 @@ import { App } from './app.tsx'
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
+    <Analytics />
   </React.StrictMode>,
 )
