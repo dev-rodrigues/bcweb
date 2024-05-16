5130e22a3d87cc5b12773d33b3546ab45ad653b5
Carlos Henrique Dos Santos Rodrigues Junior <rodrigues.carlos@ifood.com.br>
2024-05-16 09:45:55 -0300

    remove buddy

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,12 @@
-import { DevSupport } from '@react-buddy/ide-toolbox'
 import { Analytics } from '@vercel/analytics/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-
-import { ComponentPreviews, useInitial } from '@/dev'
 
 import { App } from './app.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <DevSupport
-      ComponentPreviews={ComponentPreviews}
-      useInitialHook={useInitial}
-    >
-      <App />
-    </DevSupport>
+    <App />
     <Analytics />
   </React.StrictMode>,
 )
