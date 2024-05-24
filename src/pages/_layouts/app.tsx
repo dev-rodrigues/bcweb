1f917d1a94e63a4e7c4d5908e7ab40977f76375b
Carlos Henrique Dos Santos Rodrigues Junior <rodrigues.carlos@ifood.com.br>
2024-05-24 16:49:49 -0300

    valida toke

--- src/pages/_layouts/app.tsx
+++ src/pages/_layouts/app.tsx
@@ -6,10 +6,12 @@ import { toast } from 'sonner'
 import Header from '@/components/header.tsx'
 import BarraLateral from '@/components/sidebar'
 import { useAuth } from '@/context/AuthContext.tsx'
+import { useUser } from '@/services/me-hook.ts'
 
 export function AppLayout() {
   const location = useLocation()
   const { isAuthenticated, isExpired, logout } = useAuth()
+  const { data, isError } = useUser()
 
   useEffect(() => {
     const checkTokenExpiration = setInterval(() => {
@@ -17,16 +19,36 @@ export function AppLayout() {
         console.log('expired')
         toast.error('Sua sessão expirou, faça login novamente')
         logout()
+      } else {
+        console.log('not expired')
       }
     }, 5000)
 
     return () => clearInterval(checkTokenExpiration)
-  }, [isExpired, logout])
+  }, [])
+
+  useEffect(() => {
+    if (isError) {
+      console.log('isError')
+      logout()
+      toast.error('Sua sessão expirou, faça login novamente')
+    } else {
+      console.log('no error')
+    }
+  }, [isError, data])
 
   return isAuthenticated() || isExpired() ? (
     <HStack alignItems={'start'} flexDirection={'row'}>
       <BarraLateral />
       <Stack width={'100%'} py={'6'} spacing={6}>
+        <span
+          style={{
+            display: 'none',
+          }}
+        >
+          {data?.email}
+        </span>
+
         <Header />
 
         <Outlet />
