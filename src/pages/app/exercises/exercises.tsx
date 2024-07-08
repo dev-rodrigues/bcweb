2ca83681d4324f670c2d46dd0f8817da6c08fb00
Carlos Henrique Dos Santos Rodrigues Junior <rodrigues.carlos@ifood.com.br>
2024-07-07 23:06:26 -0300

    wip tabs

--- src/pages/app/exercises/exercises.tsx
+++ src/pages/app/exercises/exercises.tsx
@@ -12,6 +12,7 @@ import {
   Thead,
   Tr,
   useBreakpointValue,
+  VStack,
 } from '@chakra-ui/react'
 import { useState } from 'react'
 import { Helmet } from 'react-helmet-async'
@@ -44,73 +45,83 @@ export function Exercises() {
         setModalOpen={setOpen}
       />
 
-      {isFetching ? (
-        <div className="mb-4 mt-4 flex justify-center">
-          <LoadingSpinner />
-        </div>
-      ) : (
-        <TableContainer
-          border={'inset'}
-          borderColor={'gray.300'}
-          borderWidth={0.5}
-          borderRadius={'5px'}
-          px={10}
-        >
-          <Table>
-            <TableHeader>
-              <Heading>Exercises</Heading>
+      {/* {isFetching ? ( */}
+      {/*  <div className="mb-4 mt-4 flex justify-center"> */}
+      {/*    <LoadingSpinner /> */}
+      {/*  </div> */}
+      {/* ) : ( */}
 
-              <Button
-                onClick={() => setOpen(true)}
-                mt={5}
-                color={'white'}
-                bg={'#E11D48'}
-                rightIcon={<Icon as={BiPlus} />}
-              >
-                Add
-              </Button>
-            </TableHeader>
-            <TableCaption>Exercises registered in the system</TableCaption>
-            <Thead>
-              <Tr>
-                {!isDrawerSidebar && <Th style={{ textAlign: 'center' }}>#</Th>}
-                <Th style={{ textAlign: 'center' }}>Nome</Th>
-                <Th style={{ textAlign: 'center' }}>Actions</Th>
-              </Tr>
-            </Thead>
-            <Tbody>
-              {data?.content.map((it, i) => {
-                return <ExerciseTableRow key={i} data={it} currentPage={page} />
-              })}
-            </Tbody>
-            <Tfoot>
+      {/* )} */}
+      <TableContainer
+        border={'inset'}
+        borderColor={'gray.300'}
+        borderWidth={0.5}
+        borderRadius={'5px'}
+        px={10}
+      >
+        <Table>
+          <TableHeader>
+            <Heading>Exercises</Heading>
+
+            <Button
+              onClick={() => setOpen(true)}
+              mt={5}
+              color={'white'}
+              bg={'#E11D48'}
+              rightIcon={<Icon as={BiPlus} />}
+            >
+              Add
+            </Button>
+          </TableHeader>
+          <TableCaption>Exercises registered in the system</TableCaption>
+          <Thead>
+            <Tr>
+              {!isDrawerSidebar && <Th style={{ textAlign: 'center' }}>#</Th>}
+              <Th style={{ textAlign: 'center' }}>Nome</Th>
+              <Th style={{ textAlign: 'center' }}>Actions</Th>
+            </Tr>
+          </Thead>
+          <Tbody>
+            {isFetching && (
               <Tr>
                 <Td colSpan={3}>
-                  <Pagination
-                    pageIndex={page}
-                    totalCount={data?.total ? data?.total : 0}
-                    perPage={10}
-                    handleNextPage={() => {
-                      setPage(page + 1)
-                    }}
-                    handlePrevPage={() => {
-                      setPage(page - 1)
-                    }}
-                    handleFirstPage={() => {
-                      setPage(0)
-                    }}
-                    handleLastPage={() => {
-                      const totalCount = data?.total ? data?.total : 0
-                      const pages = (Math.ceil(totalCount / 10) || 1) - 1
-                      setPage(pages)
-                    }}
-                  />
+                  <VStack alignItems={'center'} alignContent={'center'}>
+                    <LoadingSpinner />
+                  </VStack>
                 </Td>
               </Tr>
-            </Tfoot>
-          </Table>
-        </TableContainer>
-      )}
+            )}
+            {data?.content.map((it, i) => {
+              return <ExerciseTableRow key={i} data={it} currentPage={page} />
+            })}
+          </Tbody>
+          <Tfoot>
+            <Tr>
+              <Td colSpan={3}>
+                <Pagination
+                  pageIndex={page}
+                  totalCount={data?.total ? data?.total : 0}
+                  perPage={10}
+                  handleNextPage={() => {
+                    setPage(page + 1)
+                  }}
+                  handlePrevPage={() => {
+                    setPage(page - 1)
+                  }}
+                  handleFirstPage={() => {
+                    setPage(0)
+                  }}
+                  handleLastPage={() => {
+                    const totalCount = data?.total ? data?.total : 0
+                    const pages = (Math.ceil(totalCount / 10) || 1) - 1
+                    setPage(pages)
+                  }}
+                />
+              </Td>
+            </Tr>
+          </Tfoot>
+        </Table>
+      </TableContainer>
     </>
   )
 }
