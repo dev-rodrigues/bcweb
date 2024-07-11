1bab20e43f6712d87ca014d7fa3771aad9d3aea9
Carlos Henrique Dos Santos Rodrigues Junior <rodrigues.carlos@ifood.com.br>
2024-07-11 00:46:12 -0300

    test

--- src/pages/app/exercise-file/index.tsx
+++ src/pages/app/exercise-file/index.tsx
@@ -51,18 +51,10 @@ export function ExerciseFile() {
   }
 
   const processFile = async (it: File) => {
-    const responseAccess = await api.post<ResponseAccess>('/exercise-media', {
-      fileName: it.name,
-      contentType: it.type,
-      customerId: 2,
-      exerciseId: 2,
-    })
-
-    console.log()
-
-    const response = await axios.put(responseAccess.data.accessUrl, it, {
+    const response = await axios.put('url', it, {
       headers: {
         'Content-Type': it.type,
+        Origin: 'https://bcweb.vercel.app',
       },
     })
 
